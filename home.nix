22b7a97b0a9624cf04364416b0df15d81c4e1b72
Ren Lin <renl@kaitotlex.systems>
2025-08-30 15:30:21 -0700

    feat: some kaitoianos comfort features

--- home.nix
+++ home.nix
@@ -76,29 +76,29 @@
   #     };
   #   };
   # };
-  programs.kitty = {
-    enable = true;
-    settings = {
-      font_size = 12;
-      window_padding_width = "8 8";
-      confirm_os_window_close = -1;
-      enable_audio_bell = "no";
-      allow_remote_control = "yes";
-      listen_on = "unix:/tmp/kitty";
-      scrollback_pager = ''nvim --noplugin -c "set signcolumn=no showtabline=0" -c "silent write! /tmp/kitty_scrollback_buffer | te cat /tmp/kitty_scrollback_buffer - "'';
-      cursor = pkgs.lib.mkForce "#c0caf5";
-      cursor_text_color = pkgs.lib.mkForce "#1a1b26";
-      cursor_trail = 3;
-      #background_image = "${inputs.wallpapers}/kitty/moominResized.png";
-      background_image_layout = "cscaled";
-      #background_image_linear = "yes";
-    };
-  };
-
-  # programs.firefox = {
+  # programs.kitty = {
   #   enable = true;
-  #   package = pkgs.firefox-devedition;
+  #   settings = {
+  #     font_size = 12;
+  #     window_padding_width = "8 8";
+  #     confirm_os_window_close = -1;
+  #     enable_audio_bell = "no";
+  #     allow_remote_control = "yes";
+  #     listen_on = "unix:/tmp/kitty";
+  #     scrollback_pager = ''nvim --noplugin -c "set signcolumn=no showtabline=0" -c "silent write! /tmp/kitty_scrollback_buffer | te cat /tmp/kitty_scrollback_buffer - "'';
+  #     cursor = pkgs.lib.mkForce "#c0caf5";
+  #     cursor_text_color = pkgs.lib.mkForce "#1a1b26";
+  #     cursor_trail = 3;
+  #     #background_image = "${inputs.wallpapers}/kitty/moominResized.png";
+  #     background_image_layout = "cscaled";
+  #     #background_image_linear = "yes";
+  #   };
   # };
+
+  programs.firefox = {
+    enable = true;
+    package = pkgs.firefox-devedition;
+  };
   programs.ripgrep.enable = true;
 
   # programs.oh-my-posh = {
