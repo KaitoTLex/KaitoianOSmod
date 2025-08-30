7403d53e303b6d0c21253fe8089c988ed8d8abc1
Ren Lin <renl@kaitotlex.systems>
2025-08-30 15:18:25 -0700

    feat: some kaitoianos comfort features

---
+++ pkgs/arch/x86-64/default.nix
@@ -0,0 +1,15 @@
+{
+  pkgs,
+  ...
+}:
+with pkgs;
+[
+  arduino-cli
+  wacomtablet
+  sbctl
+  kicad
+  polychromatic
+  openrazer-daemon
+  osu-lazer-bin
+]
+#formatter."x86_64-linux" = nixpkgs.legacyPackages.x86_64-linux.nixfmt-rfc-style;
---
+++ pkgs/common.nix
@@ -0,0 +1,22 @@
+{ pkgs }:
+
+with pkgs;
+[
+
+  #goonware
+  ani-cli
+  mpv-unwrapped
+  manga-tui
+
+  #opsec
+  pass
+  tor-browser
+
+  #ee
+  openhantek6022
+
+  #hamradio
+  rtl-sdr
+  gqrx
+  soapysdr-with-plugins
+]
